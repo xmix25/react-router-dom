ad642f32b8737489aef3657fced113fff4c15d1a
Stanislau <xmix25@gmail.com>
2023-02-07 20:31:20 +0300

    add root route

--- src/index.jsx
+++ src/index.jsx
@@ -3,10 +3,12 @@ import ReactDOM from 'react-dom/client';
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import './index.css';
 
+import Root from './routes/root';
+
 const router = createBrowserRouter([
   {
     path: '/',
-    element: <div>Hello world!</div>,
+    element: <Root />,
   },
 ]);
 
