b145c49314cbcda602fbd807c4bcefd6335b78d1
Stanislau <xmix25@gmail.com>
2023-02-08 20:41:41 +0300

    global pending ui

--- src/routes/root.jsx
+++ src/routes/root.jsx
@@ -4,6 +4,7 @@ import {
   useLoaderData,
   Form,
   redirect,
+  useNavigation,
 } from 'react-router-dom';
 import { getContacts, createContact } from '../contacts';
 
@@ -19,6 +20,7 @@ export async function action() {
 
 export default function Root() {
   const { contacts } = useLoaderData();
+  const navigation = useNavigation();
   return (
     <>
       <div id='sidebar'>
@@ -69,7 +71,10 @@ export default function Root() {
           )}
         </nav>
       </div>
-      <div id='detail'>
+      <div
+        id='detail'
+        className={navigation.state === 'loading' ? 'loading' : ''}
+      >
         <Outlet />
       </div>
     </>
